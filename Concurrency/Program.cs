62ff75d8a9c9508e81f6b2e3ecf44fac5b50b7a4
Matt <sudzu97@gmail.com>
2019-02-10 19:41:22 -0500

    updated

--- Concurrency/Program.cs
+++ Concurrency/Program.cs
@@ -11,7 +11,28 @@ namespace Concurrency
 {
     class Program
     {
-        private static void Main(string[] args)
+        public static void BoxBlur(Bitmap img, ref byte[] pixels)
+        {
+            Color[,] pix = new Color[img.Width, img.Height];
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                    pix[i, j] = img.GetPixel(i, j);
+            }
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    for (var count = 0; count < pixels.Length; count += 3)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
         {
             if (args.Length != 3)
                 throw new ArgumentException("Must pass 3 arguments: image name, number of threads and number of blur passes");
@@ -21,16 +42,14 @@ namespace Concurrency
                 throw new ArgumentException("Must pass number of rounds of blurring as third argument!");
             string fname = args[0];
             Bitmap img = (Bitmap)Image.FromFile(fname);
-
+            Bitmap imgcpy = (Bitmap) Image.FromFile(fname);
             //convert image to byte array
             var bdata = img.LockBits(new Rectangle(0, 0, img.Width, img.Height),
                 ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
             byte[] pix = new byte[bdata.Stride * bdata.Height];
             Marshal.Copy(bdata.Scan0, pix, 0, pix.Length);
-            Random r = new Random();
-            //int rInt = r.Next(0, 255);
-            for (var i = 0; i < pix.Length; i++)
-                pix[i] += (byte)r.Next(0, 255);
+
+            BoxBlur(imgcpy, ref pix);
 
             //save a modified copy
             Marshal.Copy(pix, 0, bdata.Scan0, pix.Length);
